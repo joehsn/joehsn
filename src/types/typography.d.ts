3586e0c2709043305dbcf8976eb539ae3ee63f25
joehsn <joehsn@outlook.com>
2024-10-13 01:13:17 +0300

    types added

---
+++ src/types/typography.d.ts
@@ -0,0 +1,11 @@
+export interface HeadingProps extends React.HTMLProps<HTMLHeadingElement> {
+  children: React.ReactNode;
+}
+
+export interface ParagraphProps extends React.HTMLProps<HTMLParagraphElement> {
+  children: React.ReactNode;
+}
+
+export interface BlockqouteProps extends React.HTMLProps<HTMLQuoteElement> {
+  children: React.ReactNode;
+}
