c99097f48b4a53815125f5dcd247f5064d94553c
joehsn <joehsn@outlook.com>
2024-10-13 19:38:48 +0300

    Deploy to GitHub Pages using /docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/joehsn/',
+  base: '/',
+  build: {
+    outDir: 'docs'
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': resolve(__dirname, './src'),  // Alias '@' to './src'
     },
   },
 });
